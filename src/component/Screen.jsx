c0d262969bf1e743c269b273ff3a1bc828351257
charles <ketchasso72@gmail.com>
2023-06-20 11:15:13 +0100

    build react-calculator

--- src/component/Screen.jsx
+++ src/component/Screen.jsx
@@ -5,10 +5,8 @@ import { Textfit } from "react-textfit";
 export default function Screen() {
   const { calc } = useContext(CalcContext);
   return (
-    <>
-      <Textfit className="screen"  max={70} mode="single">
-        {calc.num ? calc.num : calc.res}
-      </Textfit>
-    </>
+    <Textfit className="screen" max={70} mode="single">
+      {calc.num ? calc.num : calc.res}
+    </Textfit>
   );
 }
